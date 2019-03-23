a2b875806fb1a94374251eb27738755f2a98c5ee
JustinMMiller <millerjm24@vcu.edu>
2019-03-23 08:16:47 -0400

    Moved code into a class for easier use. Also, figured out how to make the server open another connection after a previous one dies. Turns out, you just reopen it. Who'd've thunk.

--- btconn.py
+++ btconn.py
@@ -1,27 +1,32 @@
 import bluetooth
 
-host = ""
-port = 1
+class BTServer:
+	def __init__(self):
+		self.host = ""
+		self.port = 1
 
-server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-print("Created Bluetooth Socket")
+	def open(self):
+		self.server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+		print("Created Bluetooth Socket")
 
-try:
-	server.bind((host, port))
-	print("Bluetooth Binding Succeeded")
-except:
-	print("Bluetooth Binding Failed")
+	def run(self):
+		try:
+			self.server.bind((self.host, self.port))
+			print("Bluetooth Binding successful")
+		except:
+			print("Bluetooth Binding unsuccessful")
+		self.server.listen(1)
+		client, address = self.server.accept()
+		try:
+			while True:
+				data = client.recv(1024)
+				print(data)
+				client.send(data)
+		except:
+			client.close()
+			self.server.close()
 
-server.listen(1)
-
-client, address = server.accept()
-print("Connected to", address)
-print("Client", client)
-try:
-    while True:
-        data = client.recv(1024)
-        print(data)
-        client.send(data)
-except:
-    client.close()
-    server.close()
+server = BTServer()
+while True:
+	server.open()
+	server.run()
